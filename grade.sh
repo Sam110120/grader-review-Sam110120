96427bd3c2b8da452905950db3495e15d3d53256
Sam110120 <760307226@qq.com>
2023-11-27 10:59:51 -0800

    Change the bash script

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -9,8 +9,49 @@ git clone $1 student-submission
 echo 'Finished cloning'
 
 
+if [[ ! -f student-submission/ListExamples.java ]]
+then echo "Missing File"
+    exit 1
+fi
+
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/
+cd grading-area
+
+# javac -cp . ListExamples.java
+# if [ $? -ne 0 ]; then
+#     echo "Error: Compilation of ListExamples.java failed."
+#     exit 1
+# fi
+
+# javac -cp . your_grading_tests.java
+# if [ $? -ne 0 ]; then
+#     echo "Error: Compilation of grading tests failed."
+#     exit 1
+# fi
+
+# javac -cp $CPATH ListExamples.java TestListExamples.java || {
+# echo "Error"
+#     exit 1
+# }
+
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples 
+
+if [[ $? -ne 0 ]]
+then echo "0"
+else 
+echo "100"
+fi
+
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
+# grading-area
+#   |
+#   student-submission
+#       |
+#      student's ListExamples.java
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+# cp ListExamples.java grading-area
